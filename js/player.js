573e34a934b16afdfe55940aadc18d42c338b3bc
TheBeanyx <buzas.benedek.iskola@gmail.com>
2026-04-29 18:55:47 +0200

    Refactor Player class for movement controls

--- js/player.js
+++ js/player.js
@@ -7,37 +7,39 @@ class Player {
         this.height = 40;
         this.vx = 0;
         this.vy = 0;
-        this.speed = 1.5;      // Gyorsulás mértéke
+        this.speed = 1.5;      // Gyorsulás
         this.jumpForce = -15;  // Ugrás ereje
         this.grounded = false;
     }
 
     update(input) {
-        // Balra mozgás (Bal nyíl vagy A)
-        if (input.isPressed('ArrowLeft') || input.isPressed('a')) {
-            this.vx -= this.speed;
-        }
-        
-        // Jobbra mozgás (Jobb nyíl vagy D)
-        if (input.isPressed('ArrowRight') || input.isPressed('d')) {
+        // MOZGÁS IRÁNYÍTÁS:
+        // W gomb -> Előre (Jobbra a képernyőn)
+        if (input.isPressed('w') || input.isPressed('W')) {
             this.vx += this.speed;
         }
+        
+        // S gomb -> Hátra (Balra a képernyőn)
+        if (input.isPressed('s') || input.isPressed('S')) {
+            this.vx -= this.speed;
+        }
 
-        // Ugrás (Felfelé nyíl, W vagy Space) - Csak ha a földön áll!
-        if ((input.isPressed('ArrowUp') || input.isPressed('w') || input.isPressed(' ')) && this.grounded) {
+        // Space -> Ugrás (Csak ha a földön áll)
+        if (input.isPressed(' ') && this.grounded) {
             this.vy = this.jumpForce;
             this.grounded = false;
         }
+        
+        // Megjegyzés: Az A és D gombok itt nem csinálnak semmit, 
+        // azokat majd a 360° módhoz tartogatjuk.
     }
 
     draw(ctx) {
-        // A játékos egy neonkék kocka
+        // Neon kék játékos
         ctx.fillStyle = "#00f2ff";
         ctx.shadowBlur = 15;
         ctx.shadowColor = "#00f2ff";
         ctx.fillRect(this.x, this.y, this.width, this.height);
-        
-        // Shadow reset, hogy ne lassítsa a többi rajzolást
         ctx.shadowBlur = 0;
     }
 }
