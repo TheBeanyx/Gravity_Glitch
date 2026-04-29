eb89fe8f2928b75514bf89c8e92d92a47345eb10
TheBeanyx <buzas.benedek.iskola@gmail.com>
2026-04-29 17:28:41 +0200

    Enhance draw method to include UI rendering
    Added UI rendering to the draw method.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
-// js/main.js
 class NightmareEngine {
     constructor() {
         this.canvas = document.getElementById('gameCanvas');
         this.ctx = this.canvas.getContext('2d');
-        this.currentLevel = 1;
         this.lastTime = 0;
-        
-        // Alrendszerek példányosítása
+        this.currentLevel = 1;
+
         this.input = new InputHandler();
         this.physics = new Physics();
         this.renderer = new Renderer(this.ctx, this.canvas);
@@ -45,19 +43,17 @@ class NightmareEngine {
     }
 
     draw() {
-        // Háttér törlése
-        this.ctx.fillStyle = "#050505";
-        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-        
+        this.renderer.clear();
         this.ctx.save();
-        // Kamera eltolás: Csak X tengelyen (vízszintesen)
+        // Kamera eltolás csak vízszintesen
         this.ctx.translate(Math.floor(-this.camera.x), 0);
         
         if (typeof LEVEL_DATA !== 'undefined') {
             this.renderer.drawMap(LEVEL_DATA[this.currentLevel], this.camera);
         }
         this.player.draw(this.ctx);
-        
         this.ctx.restore();
+        
+        this.renderer.drawUI(this.player);
     }
 }
