2f40792f5e5b5696ddf89d88d52f2625fb9425ee
TheBeanyx <buzas.benedek.iskola@gmail.com>
2026-04-29 18:49:30 +0200

    Refactor InputHandler to use object for key states

--- js/input.js
+++ js/input.js
@@ -1,38 +1,20 @@
-/**
- * NIGHTMARE ENGINE - Input Handler Module
- * Felelős a billentyűzet események aszinkron kezeléséért.
- */
 class InputHandler {
     constructor() {
-        // A lenyomott gombok tárolása (gyorsabb, mint a tömb)
-        this.keys = new Set();
+        this.keys = {};
         
-        // Eseményfigyelők regisztrálása
+        // Figyeljük a gombnyomást
         window.addEventListener('keydown', (e) => {
-            const key = e.key.toUpperCase();
-            this.keys.add(key);
-            
-            // Debug mód: konzolra írjuk a leütést, ha szükséges
-            // console.log(`INPUT_SYSTEM: Key ${key} registered.`);
+            this.keys[e.key] = true;
         });
 
+        // Figyeljük a gomb felengedését
         window.addEventListener('keyup', (e) => {
-            const key = e.key.toUpperCase();
-            this.keys.delete(key);
-        });
-
-        // Fókuszvesztés esetén ürítjük a gombokat (megakadályozza a beragadást)
-        window.addEventListener('blur', () => {
-            this.keys.clear();
+            this.keys[e.key] = false;
         });
     }
 
-    /**
-     * Ellenőrzi, hogy egy adott gomb le van-e nyomva.
-     * @param {string} keyCode 
-     * @returns {boolean}
-     */
-    isPressed(keyCode) {
-        return this.keys.has(keyCode.toUpperCase());
+    // Segédfüggvény, hogy kényelmesebb legyen lekérdezni
+    isPressed(key) {
+        return this.keys[key] === true;
     }
 }
