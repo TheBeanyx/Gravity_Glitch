10da90bb8a974939428c62207d110ed1265410e8
TheBeanyx <buzas.benedek.iskola@gmail.com>
2026-04-29 19:09:07 +0200

    Refactor Player class with new initial values
    Updated player starting position and movement speed.

--- js/player.js
+++ js/player.js
@@ -1,45 +1,37 @@
 class Player {
     constructor(game) {
         this.game = game;
-        this.x = 0;
-        this.y = 0;
+        this.x = 100;
+        this.y = 100;
         this.width = 40;
         this.height = 40;
         this.vx = 0;
         this.vy = 0;
-        this.speed = 1.5;      // Gyorsulás
-        this.jumpForce = -15;  // Ugrás ereje
+        this.speed = 1.2;
+        this.jumpForce = -15;
         this.grounded = false;
     }
 
     update(input) {
-        // MOZGÁS IRÁNYÍTÁS:
-        // W gomb -> Előre (Jobbra a képernyőn)
-        if (input.isPressed('w') || input.isPressed('W')) {
+        // W gomb (KeyW) -> Jobbra
+        if (input.isPressed('KeyW')) {
             this.vx += this.speed;
         }
         
-        // S gomb -> Hátra (Balra a képernyőn)
-        if (input.isPressed('s') || input.isPressed('S')) {
+        // S gomb (KeyS) -> Balra
+        if (input.isPressed('KeyS')) {
             this.vx -= this.speed;
         }
 
-        // Space -> Ugrás (Csak ha a földön áll)
-        if (input.isPressed(' ') && this.grounded) {
+        // Space -> Ugrás
+        if (input.isPressed('Space') && this.grounded) {
             this.vy = this.jumpForce;
             this.grounded = false;
         }
-        
-        // Megjegyzés: Az A és D gombok itt nem csinálnak semmit, 
-        // azokat majd a 360° módhoz tartogatjuk.
     }
 
     draw(ctx) {
-        // Neon kék játékos
         ctx.fillStyle = "#00f2ff";
-        ctx.shadowBlur = 15;
-        ctx.shadowColor = "#00f2ff";
         ctx.fillRect(this.x, this.y, this.width, this.height);
-        ctx.shadowBlur = 0;
     }
 }
